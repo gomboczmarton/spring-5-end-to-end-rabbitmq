7013c00021f63d37d43e7a4ea556c62211e520ab
Marton Gombocz <gomboczmarton@gmail.com>
2019-07-12 11:20:50 +0200

    customer notificatzion service with fake smtp

---
+++ customernotificationservice/src/main/java/biz/gomboczmarton/microservice/customernotificationservice/CustomernotificationserviceApplication.java
@@ -0,0 +1,13 @@
+package biz.gomboczmarton.microservice.customernotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomernotificationserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomernotificationserviceApplication.class, args);
+    }
+
+}
---
+++ customernotificationservice/src/test/java/biz/gomboczmarton/microservice/customernotificationservice/CustomernotificationserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package biz.gomboczmarton.microservice.customernotificationservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomernotificationserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
